ad6501001f90f70b20f2df4ddf0a1652b1280d03
Rafael <rafael.sousadrd@gmail.com>
2024-06-23 16:18:42 -0300

    criando o mobile

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
     const question = document.querySelectorAll('[data-faq-question]');
-    
 
+    const heroSection = document.querySelector('.hero');
+    const  alturaHero = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function() {
+        const posicaoAtual = window.scrollY;
+
+        if(posicaoAtual < alturaHero) {
+            ocultaElementosHeader();
+        }else{
+            exibeElementosHeader();
+        }
+    })
+    
+    //Seção  de atrações, programação das abas dinamicas
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao){
             const abaAlvo = botao.target.dataset.tabButton;
@@ -19,6 +32,19 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 })
 
+//Ocultando elementos do header
+function ocultaElementosHeader(){
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+//Exibindo elementos do header
+function exibeElementosHeader(){
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+}
+
+//Seção Faq, accordion
 function abreOuFechaResposta(elemento) {
     const classe = 'faq__questions__item--is-open';
     const elementoPai = elemento.target.parentNode;
